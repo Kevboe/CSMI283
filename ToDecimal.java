a2b1282dfb576bbafb5f0842363903a19b8bbbae
Kevin <thekev33@gmail.com>
2017-10-03 15:55:56 -0700

    Need to figure out second variable
    Code should work, but I don't understand how the second variable is passed into the code. Once I get the second variable working, which is a string, then the code will be finished.

--- ToDecimal.java
+++ ToDecimal.java
@@ -1,17 +1,31 @@
 public class ToDecimal{
   int base;
-  int numInBase;
+  ArrayList<Integer> numInBase;
   int convertedNum;
+
+
+  //2nd Variable is put in via standard input. Standard Input is Keyboard.
   public static void main(String[] args) {
-    base = Integer.valueOf(args[0]);
+    base = Integer.parseInt(args[0]);
     /* METHOD WON'T WORK FOR BASES LARGER THAN 9.
     NEED TO SEE HOW OTHERVARIABLE IS PASSED IN TO SOLVE*/
-    for(int x = OTHERVARIABLE.length(); x > 0; x++){
-      int pwr = 0;
-      int convertedNum = 0;
-      if(OTHERVARIABLE.charAt(x) != '[' || OTHERVARIABLE.charAt(x) != ']' ){
-        convertedNum += (Integer.parseInt("" + OTHERVARIABLE.charAt(x))) * Math.pow( base, pwr);
+    numInBase = new ArrayList<Integer>;
+    for(int x = 0; x < OTHERVARIABLE.length(); x++){
+      if(OTHERVARIABLE.charAt(x) == '['){
+        int y = x;
+        do {
+          y++;
+        } while (OTHERVAIABLE.charAt(y) != ']');
+        numInBase.add(OTHERVAIABLE.substring(x+1, y))
       }
+      //Add a println to test the ArrayList creation
+    }
+
+    int pwr = 0;
+    convertedNum = 0;
+    for(int x = numInBase.size() - 1; x > 0; x++){
+      convertedNum += numInBase.get(x) * (Math.pow(base, pwr));
+      pwr++;
     }
     System.out.println(convertedNum);
   }
